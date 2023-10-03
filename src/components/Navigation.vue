87e31a60a3f9dc0febaad133843940bbc0036a1c
benadidafouzia <benadidafouzia@gmail.com>
2023-10-03 17:01:19 +0300

    j'en ai marre

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar app class="bg-background-dark text-light"  color="#37474F" dark>
+    <v-app-bar app class="bg-background-dark text-light" color="#37474F" dark>
       <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Animated Navbar</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-col >
-    
-        <v-btn class="icon-button md:flex hidden" icon>
-          <v-icon fill="dark">mdi-account</v-icon>
+      <v-col>
+        <v-btn
+          class="icon-button md:flex hidden"
+          icon="mdi-account"
+        @click="goToUser"
+        >
         </v-btn>
         <v-btn icon class="icon-button md:flex hidden custom-btn">
-          <v-icon>mdi-cart-outline</v-icon>
+          <v-icon class="md:flex hidden">mdi-cart-outline</v-icon>
         </v-btn>
       </v-col>
     </v-app-bar>
@@ -27,14 +29,22 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+  
   </v-app>
 </template>
 
 <script lang="ts">
+import UserDialog from "./dialogs/UserPage.vue";
 export default {
+  name: "Navigation",
+  components: {
+    UserDialog,
+  },
   data() {
     return {
       drawer: false,
+      menu: false,
+      openDialog: false,
       items: [
         { text: "Home", icon: "mdi-home" },
         { text: "About", icon: "mdi-information" },
@@ -47,13 +57,17 @@ export default {
     toggleDrawer() {
       this.drawer = !this.drawer;
     },
+    goToUser() {
+    this.$router.push({ path: "userpage" });
   },
+  },
+
 };
 </script>
 
 <style scoped>
 .custom-btn:hover {
-  background-color: #FFC107; 
-  color: #000000; 
+  background-color: #ffc107;
+  color: #000000;
 }
 </style>
