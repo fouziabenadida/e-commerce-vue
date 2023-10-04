72954d403ad3a6c4862a9762d12bef067fab06be
benadidafouzia <benadidafouzia@gmail.com>
2023-10-04 10:43:54 +0300

    refactor

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,22 +2,56 @@
   <v-app>
     <v-app-bar app class="bg-background-dark text-light" color="#37474F" dark>
       <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Animated Navbar</v-toolbar-title>
+      <v-toolbar-title
+        style="
+          font-size: large;
+          font-family: Georgia, 'Times New Roman', Times, serif;
+        "
+        >Trabajando</v-toolbar-title
+      >
       <v-spacer></v-spacer>
       <v-col>
         <v-menu>
           <template v-slot:activator="{ props }">
-            <v-btn   v-bind="props" class="icon-button md:flex hidden" icon="mdi-account">
+            <v-btn
+              v-bind="props"
+              class="icon-button md:flex hidden"
+              icon="mdi-account"
+              style="background-color: #37474f"
+            >
             </v-btn>
           </template>
-          <v-list>
-            <v-list-item>
-           <v-btn>haha</v-btn>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>aa</v-list-item-title>
-            </v-list-item>
-          </v-list>
+          <v-card
+            class="overflow-hidden"
+            width="250px"
+            height="20vh"
+            style="background-color: #37474f; padding: 0"
+            :border="false"
+          >
+            <v-card-title class="text-center">
+              <v-btn
+                width="100%"
+                max-height="100px"
+                color="#F5AB3D"
+                class="pa-2 mt-2 custom-btn"
+                >Log in</v-btn
+              >
+            </v-card-title>
+            <p
+              class="ma-1 text-center"
+              style="font-size: 16px; font-family: sans-serif; color: #cccccc"
+            >
+              Dont' have an account yet?
+            </p>
+            <v-card-title class="text-center">
+              <v-btn
+                width="100%"
+                style="color: #f5ab3d; border: #f5ab3d"
+                class="pa-2 mt-2 register-btn"
+                >Register</v-btn
+              >
+            </v-card-title>
+          </v-card>
         </v-menu>
 
         <v-btn icon class="icon-button md:flex hidden custom-btn">
@@ -42,12 +76,9 @@
 </template>
 
 <script lang="ts">
-import UserDialog from "./dialogs/UserPage.vue";
 export default {
   name: "Navigation",
-  components: {
-    UserDialog,
-  },
+  components: {},
   data() {
     return {
       drawer: false,
@@ -76,5 +107,11 @@ export default {
 .custom-btn:hover {
   background-color: #ffc107;
   color: #000000;
+  border-radius: 1px;
+}
+.register-btn:hover {
+  background-color: #ffc107;
+  color: #000000 !important;
+  border-radius: 1px;
 }
 </style>
